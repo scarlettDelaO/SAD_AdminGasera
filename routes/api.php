791b0ed581377a4a2daf3e3ea5755efb2d7dc3a0
DairaDV <daira.demillon@gmail.com>
2024-03-10 23:43:08 -0600

    Changes to be committed: 	modified:   routes/api.php 	modified:   routes/web.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/vendedores', 'App\Http\Controllers\VendedorController@index');
+Route::post('/vendedores', 'App\Http\Controllers\VendedorController@index');
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VendedorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('vendedors', '@index');
+Route::post('vendedors', '@index');
